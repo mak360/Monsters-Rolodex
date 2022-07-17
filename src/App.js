4c71a6a7cf87185260bce40d9371f342a57dc823
MAK <mak2022official@gmail.com>
2022-07-18 00:10:48 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,58 @@
+import { Component } from 'react';
+
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor(){
+    super();
+
+    this.state = {
+      monsters: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then((response) => response.json())
+    .then((users) => this.setState(
+      () => { 
+        return {monsters: users}
+      }, 
+      () => {
+        console.log(this.state)
+      })
+    )
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <input 
+          className='search-box' 
+          type="search" 
+          placeholder='Search Monster' 
+          onChange={(event) => {
+            console.log(event.target.value);
+            const searchString = event.target.value.toLocaleLowerCase();
+            const filteredMonster = this.state.monsters.filter((monster) => {
+              return monster.name.toLocaleLowerCase().includes(searchString);
+            });
+
+            this.setState(() => {
+              return { monsters: filteredMonster }
+            });
+            }} />
+        {this.state.monsters.map((monster) => {
+          return(
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          ) 
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
